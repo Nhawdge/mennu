930794723d7be61419d0ec53ea2478844071a436
John Pavek <JohnQPavek@gmail.com>
2018-12-31 12:08:28 -0600

    Added VueJS

---
+++ src/vue.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#meal-planner',
+    data : {
+        message:'Hello',
+        suggestions: [
+            {text: "Spaghetti"},
+            {text: "Broccoli"},
+            {text: "Peas"},
+            {text: "Carrots"}
+        ]
+    }
+})
