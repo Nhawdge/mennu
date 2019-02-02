22a64e961bf209e2560bbd264163ae130e731516
John Pavek <JohnQPavek@gmail.com>
2019-02-01 23:58:44 -0600

    Just a bunch of changes to the file structure. I added express Also working on converting vue to TS and hopefully it'll be awesome

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,80 +1,109 @@
-const http = require('http');
+//const http = require('http');
+//const fs = require('fs');
+
 const hostname = '127.0.0.1';
 const port = 3000;
-const fs = require('fs');
+
+import express = require("express");
 import { api } from "./api";
 
 export default class {
   static server() {
-    return http.createServer((req, res) => {
-      console.log(req.method, "Request from:", req.url);
+    const app: express.Application = express();
 
-      res.statusCode = 200;
-      var apiCall = router(req.url);
+    app.get('/', function (req: Request, res: any) {
+      res.sendFile("src/index.html", { root: '.' });
+    })
+ 
+    app.use(express.static("src/web/templates"))
+    app.use("/assets/", express.static("src/web/assets"))
+    app.use("/web", express.static("build/web"))
 
-      if (req.method == 'GET') {
-        apiCall(function (result) {
-          res.end(result)
-        })
-      }
-      else if (req.method == 'POST') {
-        console.log(`'${req.data}'`);
-        var body = "";
-
-        req.on('data', function (data) {
-          body += data;
-        });
-
-        req.on('end', function (data) {
-          if (data) {
-            body += data;
-          }
-
-          console.log("body is: ", body);
-          apiCall(JSON.parse(body), function (result) {
-            res.end(result)
-          })
-        });
-      }
+    app.listen(3000, function () {
+      console.log(`Server has started at http://${hostname}:${port} `);
     });
   };
   static start() {
-    this.server().listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-    });
-  }
+    this.server();
+  };
 }
 
-function router(path): Function {
-  switch (path) {
-    case '/meals':
-      return api.meals.getAll;
-      break;
-    case '/meals/add':
-      return api.meals.add;
-      break;
-    case '/meals/save':
-      return api.meals.edit;
-      break;
-    case '/ingredients':
-      return api.ingredients.getAll;
-      break;
-    default:
-      return serveHtml(path);
-      break;
-  }
-}
+  // static server() {
+  //   return http.createServer((req, res) => {
+  //     console.log(req.method, "Request from:", req.url);
 
-function serveHtml(path) {
-  return function (callback) {
-    if (path == '/') { path = '/index.html' }
-    fs.readFile("./src" + path, "utf8", function (err, data) {
-      if (data) {
-        callback(data);
-      }
-      else {
-        console.warn("File not found");
-      }
-    })
-  }
-}
+  //     res.statusCode = 200;
+  //     var apiCall = router(req.url);
+
+  //     if (req.method == 'GET') {
+  //       apiCall(function (result) {
+  //         res.end(result)
+  //       })
+  //     }
+  //     else if (req.method == 'POST') {
+  //       console.log(`'${req.data}'`);
+  //       var body = "";
+
+  //       req.on('data', function (data) {
+  //         body += data;
+  //       });
+
+  //       req.on('end', function (data) {
+  //         if (data) {
+  //           body += data;
+  //         }
+
+  //         console.log("body is: ", body);
+  //         apiCall(JSON.parse(body), function (result) {
+  //           res.end(result)
+  //         })
+  //       });
+  //     }
+  //   });
+  // };
+  // static start() {
+  //   this.server().listen(port, hostname, () => {
+  //     console.log(`Server running at http://${hostname}:${port}/`);
+  //   });
+  // }
+
+// function router(path): Function {
+//   switch (path) {
+//     case '/meals':
+//       return api.meals.getAll;
+//       break;
+//     case '/meals/add':
+//       return api.meals.add;
+//       break;
+//     case '/meals/save':
+//       return api.meals.edit;
+//       break;
+//     case '/ingredients':
+//       return api.ingredients.getAll;
+//       break;
+//     default:
+//       return serveHtml(path);
+//       break;
+//   }
+// }
+
+// function serveHtml(path) {
+//   return function (callback) {
+//     if (path == '/') { path = '/index.html' }
+//     path = "./src" + path;
+//     if (!fs.existsSync(path)) {
+//       path.replace("./src", "./build");
+//     }
+
+//     fs.readFile(path, "utf8", function (err, data) {
+//       if (data) {
+//         callback(data);
+//       }
+//       else {
+
+//         callback("File not found")
+//         console.warn("File not found");
+//       }
+//     })
+//   }
+// }
---
+++ src/web/app.ts
@@ -0,0 +1,12 @@
+
+import vue from "vue";
+import menu from "./menu"
+
+
+vue.component('test', {
+    data: function () {
+        return { 'test': 'test' }
+    }
+    ,
+    template: 'test test test'
+})
